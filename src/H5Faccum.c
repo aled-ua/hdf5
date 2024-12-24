ff56cbcad6ff3543ebf536f8e5004a6c08ddeed8
aled-ua <bugbuster.cc@gmail.com>
2024-12-24 07:46:41 +0000

    Fix vuln OSV-2024-381

--- src/H5Faccum.c
+++ src/H5Faccum.c
@@ -881,6 +881,11 @@ H5F__accum_free(H5F_shared_t *f_sh, H5FD_mem_t H5_ATTR_UNUSED type, haddr_t addr
                 H5_CHECKED_ASSIGN(overlap_size, size_t, (addr + size) - accum->loc, haddr_t);
                 new_accum_size = accum->size - overlap_size;
 
+                /* Ensure overlap_size and new_accum_size are within bounds */
+                if (overlap_size > accum->alloc_size || new_accum_size > accum->alloc_size) {
+                    HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "calculated sizes exceed allocated buffer size");
+                }
+
                 /* Move the accumulator buffer information to eliminate the freed block */
                 memmove(accum->buf, accum->buf + overlap_size, new_accum_size);
 
