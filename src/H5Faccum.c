0d7b9d68ef359bd50080eb2806f0bb962ac67b75
aled-ua <bugbuster.cc@gmail.com>
2025-01-09 11:01:22 +0800

    Fix typo err

--- src/H5Faccum.c
+++ src/H5Faccum.c
@@ -885,7 +885,7 @@ H5F__accum_free(H5F_shared_t *f_sh, H5FD_mem_t H5_ATTR_UNUSED type, haddr_t addr
                 if (H5_IS_BUFFER_OVERFLOW(accum->buf + overlap_size, new_accum_size,
                                           accum->buf + accum->alloc_size)) {
                     HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "memmove operation would overflow buffer");
-
+                }
                 /* Move the accumulator buffer information to eliminate the freed block */
                 memmove(accum->buf, accum->buf + overlap_size, new_accum_size);
 
