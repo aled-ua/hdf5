d3ea1d8536523e4f5e5c08dd0f27d71327f0ecc2
aled-ua <bugbuster.cc@gmail.com>
2025-01-09 10:20:52 +0800

    use H5_IS_BUFFER_OVERFLOW to check overflow

--- src/H5Cimage.c
+++ src/H5Cimage.c
@@ -1288,7 +1288,7 @@ H5C__decode_cache_image_header(const H5F_t *f, H5C_t *cache_ptr, const uint8_t *
     p = *buf;
 
     /* Ensure buffer has enough data for signature comparison */
-    if ((size_t)(*buf + H5C__MDCI_BLOCK_SIGNATURE_LEN - p) > cache_ptr->image_len)
+    if (H5_IS_BUFFER_OVERFLOW(p, H5C__MDCI_BLOCK_SIGNATURE_LEN, *buf + cache_ptr->image_len))
         HGOTO_ERROR(H5E_CACHE, H5E_OVERFLOW, FAIL, "Insufficient buffer size for signature");
 
     /* Check signature */
